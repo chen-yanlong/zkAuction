00a19d991d2e5dde0b5d72e78ad929acbdae20b5
chen-yanlong <alan900801@gmail.com>
2024-08-15 02:45:06 +0800

    imply Inc. merkle tree

--- contracts/contracts/Auction.sol
+++ contracts/contracts/Auction.sol
@@ -1,22 +1,29 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity >=0.8.0;
 
-import "./PoseidonT3.sol";
+import "@zk-kit/incremental-merkle-tree.sol/IncrementalBinaryTree.sol";
 
 // Please note that you should adjust the length of the inputs
 interface IVerifier {
     function verifyProof(
-        
+        uint256[2] memory a,
+        uint256[2] memory b,
+        uint256[2] memory c,
+        uint256[3] memory publicSignals
     ) external view returns (bool r);
 }
 
 contract Auction {
+    using IncrementalBinaryTree for IncrementalTreeData;
+
     struct Proof {
         
     }
 
+    event TreeCreated(bytes32 id, uint256 depth);
+    event LeafInserted(uint256 leaf, uint256 root);
     event Start(uint256 startTime, uint256 endTime);
-    event Bid(address indexed bidder);
+    event Bid(address bidder, bytes32 encryptedData);
     event End();
     event Tally(address winner);
 
@@ -30,16 +37,19 @@ contract Auction {
     uint256 public endTime;
 
     //merkle tree
-    mapping(address => uint256) public bidderIdx; 
+    mapping(uint256 => address) public bidder; 
     mapping(uint256 => bytes32) public leaves; 
     uint256 public leafCount = 0;
     bytes32 public root;
+    IncrementalTreeData public tree;
 
     //verifier
     address public immutable verifier;
     
 
-    constructor(address verifier_, uint256 duration_) {
+    constructor(address verifier_, uint256 duration_, uint256 treeDepth_) {
+        tree.init(treeDepth_, 0);
+        
         verifier = verifier_;
         _owner = msg.sender;
         duration = duration_;
@@ -57,30 +67,27 @@ contract Auction {
 
     function bid(bytes32 _encryptedBid) public {
         require(block.timestamp < endTime, "Bidding time has ended.");
-        require(bidderIdx[msg.sender] == 0, "User already submitted bid");
-        bidderIdx[msg.sender] = leafCount;
+        bidder[leafCount] = msg.sender;
         leaves[leafCount] = _encryptedBid;
         leafCount++;
-        emit Bid(msg.sender);
+        tree.insert(_encryptedBid); 
+        emit Bid(msg.sender, _encryptedBid);
+        emit LeafInserted(_encryptedBid, tree.root);
     }
 
 
     function endAuction(bytes32 _winningProof, address _winner) public {
         require(block.timestamp >= endTime, "Bidding time not yet ended.");
         require(!ended, "Auction already ended.");
-
-        bytes32 currentHash = leaves[0];
-        for (uint256 i = 1; i < leafCount; i++) {
-            currentHash = keccak256(abi.encodePacked(currentHash, leaves[i]));
-        }
         
-        root = currentHash;
+        root = tree.root;
         ended = true;
         emit End();
     }
 
 
-    function tally(uint256[3] memory publicSignals, Proof memory proof) public {
+    function tally(uint256[3] memory publicSignals, Proof memory proof, uint256 winnerIdx) public {
+        require(msg.sender == _owner, "Only owner can tally.");
         bool result = IVerifier(verifier).verifyProof(
             proof.a,
             proof.b,
@@ -88,27 +95,10 @@ contract Auction {
             publicSignals
         );
         
-        require(verify(_winningProof), "Invalid proof.");
+        require(result == 1, "Invalid proof.");
 
-        winner = _winner;
+        winner = bidder[winnerIdx];
     }
 
-
-    /**
-     * Please adjust the IVerifier.sol and the array length of publicSignals
-     */
-    function verify(uint256[3] memory publicSignals, Proof memory proof)
-        public
-        view
-        returns (bool)
-    {
-        bool result = IVerifier(verifier).verifyProof(
-            proof.a,
-            proof.b,
-            proof.c,
-            publicSignals
-        );
-        return result;
-    }
 
 }
